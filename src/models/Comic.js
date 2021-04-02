d18c43cac3cd68a2631e26c1f484c97cccdbb8c3
joopedroe <joopedroe936@mail.com>
2021-04-02 20:07:03 -0300

    feat: adicionar model comic

---
+++ src/models/Comic.js
@@ -0,0 +1,34 @@
+const model = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ComicSchema = new Schema({
+    id:{
+        type: Number,
+        required:true,
+    },
+    title:{
+        type: String,
+        required:true,
+    },
+    description:{
+        type: String,
+        required:false,
+    },
+    imageURL:{
+        type:String,
+        required:true,
+    },
+    type_image:{
+        type: String,
+        required:true,
+    },
+    id_user:{ 
+        type: Schema.Types.ObjectId, ref: 'User' 
+    },
+   
+},{
+    timestamps:true
+},);
+
+module.exports = mongoose.model('Comic', ComicSchema);
