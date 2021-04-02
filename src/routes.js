4d402d080d7df0a2b344cf4fa53a7804ed260901
joopedroe <joopedroe936@mail.com>
2021-04-02 20:23:46 -0300

    feat: função de adicionar comic

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const {Router} =require('express');
 const UserController = require('./controllers/user-controller');
 const authController = require('./controllers/auth-controller.js');
+const comicController = require('./controllers/comic-controller')
 
 const routes = new Router();
 
 routes.post('/login',authController.Autenticacao);
 routes.post('/novo/usuario',UserController.newUser);
 routes.put('/atualizar/usuario',UserController.updateUser);
+routes.post('/comic/adicionar',comicController.add_comic)
 module.exports = routes
