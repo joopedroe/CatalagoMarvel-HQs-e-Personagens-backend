905f7097952cee30036d312fd068a32e3f717747
joopedroe <joopedroe936@mail.com>
2021-04-02 21:29:28 -0300

    feat: adicionar model character

---
+++ src/models/Characters.js
@@ -0,0 +1,34 @@
+const model = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CharacterSchema = new Schema({
+    id:{
+        type: Number,
+        required:true,
+    },
+    name:{
+        type: String,
+        required:true,
+    },
+    description:{
+        type: String,
+        required:false,
+    },
+    imageURL:{
+        type:String,
+        required:true,
+    },
+    type_image:{
+        type: String,
+        required:true,
+    },
+    id_user:{ 
+        type: Schema.Types.ObjectId, ref: 'User' 
+    },
+   
+},{
+    timestamps:true
+},);
+
+module.exports = mongoose.model('Character', CharacterSchema);
